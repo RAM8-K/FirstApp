82ead60bc9633122f67b5156ae6fa95723594b05
RAM8-K <remkadyrov@icloud.com>
2024-11-12 22:18:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.WebSockets;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Sasha";
+            Console.WriteLine(MyName);
+        }
+    }
+}
